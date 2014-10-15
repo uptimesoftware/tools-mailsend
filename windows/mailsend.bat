a1732918745fcda77e352d30f5c5ccdba3dee49c
Abdullah Salim <salim.abdullah@gmail.com>
2014-10-15 17:11:58 -0400

    tested on windows

---
+++ windows/mailsend.bat
@@ -0,0 +1,19 @@
+rem This utility uses Mailsend to send emails using TLS.  For more information on Mailsend, see https://github.com/muquit/mailsend
+
+rem Generate email body
+rem Additional parameters for body can be found here - http://docs.uptimesoftware.com/display/UT/Alert+Profile+and+Action+Profile+Variables#AlertProfileandActionProfileVariables-ScriptAlertVariables
+
+del email.txt
+echo Notification Type: %UPTIME_ALERTTYPE% >> email.txt
+echo Date: %UPTIME_DATETIME% >> email.txt
+echo. >> email.txt
+echo Host: %UPTIME_HOSTNAME% >> email.txt
+echo Host Status: %UPTIME_HOSTSTATUS% >> email.txt
+echo Service: %UPTIME_SVCNAME% >> email.txt
+echo Service Status: %UPTIME_SVCSTATUS% >> email.txt
+echo. >> email.txt
+echo Service Output: %UPTIME_SVCOUTPUT% >> email.txt
+
+set SMTPSENDERNAME="up.time Alert"
+
+mailsend1.17b15.exe -to %UPTIME_EMAIL% -name %SMTPSENDERNAME% -from %UPTIME_SMTPSENDER% -starttls -smtp %UPTIME_SMTPSERVER% -port %UPTIME_SMTPPORT% -sub %UPTIME_SUBJECT% -msg-body email.txt +cc +bc -q -auth-plain -user %UPTIME_SMTPUSER% -pass %UPTIME_SMTPPASSWORD%
